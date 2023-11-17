50495a5a2a6d5bb05990ee0c1de7cef87a132f81
HillarySoto <hillary.sotosoto@ucr.ac.cr>
2023-11-16 22:40:47 -0600

    Subiendo nuevos cambios

--- OgilviesMakeUpModulos/OgilviesMakeUpModulos/OgilviesMakeUpModulos/Models/Producto.cs
+++ OgilviesMakeUpModulos/OgilviesMakeUpModulos/OgilviesMakeUpModulos/Models/Producto.cs
@@ -1,13 +1,19 @@
-namespace OgilviesMakeUpModulos.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace OgilviesMakeUpProject.Models;
 
 public partial class Producto
 {
-    public int IdProducto { get; set; }
-    public string Nombre { get; set; }
-    public string Imagen { get; set; }
-    public int Precio { get; set; }
-    public string Categorias { get; set; }
-    public string SubCategoria { get; set; }
-    public string Marca { get; set; }
-    public string Categoria { get; set; }
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int? Precio { get; set; }
+
+    public string? Color { get; set; }
+
+    public string? Marca { get; set; }
+
+    public string? Categoria { get; set; }
 }

12c7a895c56ba81600726c1e7e934f5b2aa4bbd1
José Jiménez <josar.menez@gmail.com>
2023-12-04 16:30:02 -0600

    Pedidos y carrito

---
+++ ProyectoAnalisis/ClientApp/src/components/TablaCarrito.jsx
@@ -0,0 +1,82 @@
+﻿/* eslint-disable eqeqeq */
+import React, { useState } from 'react';
+import { Table, Button, } from "reactstrap";
+
+
+const TablaCarrito = ({ data, setEditarCarrito, mostrarModalCarrito, setMostrarModalCarrito, deleteCarrito, user }) => {
+
+
+    //PARA PAGINACIÓN
+
+    const itemsPerPage = 10;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+
+    //PARA LOS DATOS
+
+    const enviarDatos = (carrito) => {
+        setEditarCarrito(carrito);
+        setMostrarModalCarrito(!mostrarModalCarrito);
+    };
+
+
+    return (
+        <div>
+            <Table striped responsive>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Producto</th>
+                        <th>Marca</th>
+                        <th>Cantidad</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {currentItems.length < 1 ? (
+                        <tr>
+                            <td colSpan="9">Sin REGISTROS</td>
+                        </tr>
+                    ) : (
+                            console.log(data),
+                            currentItems.filter((item) => item.idCliente == user.id) // Filtrar por idCliente igual a user.id
+                                .map((item, index)=> (
+                            <tr key={item.id}>
+                                <td>{index + 1 + (currentPage - 1) * itemsPerPage}</td>
+                                <td>{item.nombreProducto}</td>
+                                <td>{item.marcaProducto}</td>
+                                <td>{item.cantidad}</td>
+                                <td>
+                                    <Button color="primary" size="sm" className="me-2" onClick={() => enviarDatos(item)}> Editar </Button>
+                                    <Button color="danger" size="sm" onClick={() => deleteCarrito(item.id)}> Eliminar </Button>
+                                </td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </Table>
+            <div>
+                {data.length > itemsPerPage && (
+                    <ul className="pagination">
+                        {[...Array(Math.ceil(data.length / itemsPerPage))].map((_, index) => (
+                            <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                                <button className="page-link" onClick={() => paginate(index + 1)}>
+                                    {index + 1}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default TablaCarrito;
---
+++ ProyectoAnalisis/ClientApp/src/services/CarritoServicio.js
@@ -0,0 +1,144 @@
+import Swal from "sweetalert2";
+
+export const mostrarCarrito = async (id) => { //para monstrar el inventario
+
+    const response = await fetch("api/Carrito/ListarCarrito/" +id);
+
+    try {
+        if (response.ok) {
+            const data = await response.json();
+            return data; //listado de inventario
+
+        } else {
+            console.log("error al obtener la lista")
+            return [];
+        }
+    } catch (error) {
+        console.error("Error en la solicitud fn mostrarCarrito", error);
+    }
+};
+
+
+
+export const addToCart = async (objeto) => {
+
+    try {
+
+        await fetch("api/Carrito/Guardar", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(objeto),
+        });
+
+    } catch (error) {
+        console.error("Error al agregar al carrito:", error);
+        throw error;
+    } finally {
+       //mostrarDetallesPedidos();
+    }
+};
+
+
+export const editarProductoCarrito = async (objeto) => {
+    try {
+        const result = await Swal.fire({
+            title: 'Actualizar la Informaci&oacuten?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'Cancelar',
+            allowOutsideClick: false,
+        });
+
+        if (result.isConfirmed) {
+            await fetch("api/Carrito/Editar", {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                },
+                body: JSON.stringify(objeto),
+            });
+        }
+    } catch (error) {
+        console.error("Error al editar la entrada de Carrito:", error);
+        throw error;
+    } finally {
+      //  mostrarDetallesPedidos();
+    }
+};
+
+
+
+
+
+
+
+
+export const eliminarProductoCarrito = async (id) => {
+    const respuesta = await Swal.fire({
+        title: 'Eliminar el producto del carrito?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        allowOutsideClick: false,
+    });
+
+    if (respuesta.isConfirmed) {
+        try {
+            await fetch(`api/Carrito/Eliminar/${id}`, {
+                method: 'DELETE',
+            });
+        } catch (error) {
+            console.error("Error al eliminar el Carrito:", error);
+            throw error;
+        } finally {
+         //   mostrarDetallesPedidos();
+        }
+    }
+};
+
+
+export const listarProductos = async () => { //para monstrar el inventario
+
+    const response = await fetch("api/Producto/Lista");
+
+    try {
+        if (response.ok) {
+            const data = await response.json();
+            return data; //listado de inventario
+
+        } else {
+            console.log("error al obtener la lista")
+            return [];
+        }
+    } catch (error) {
+        console.error("Error en la solicitud fn listarProductos", error);
+    }
+};
+
+
+
+
+export const crearPedidoDesdeCarrito = async (idCliente) => {
+    try {
+        const response = await fetch(`api/Carrito/CrearPedidoDesdeCarrito/${idCliente}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            console.log('Pedido creado exitosamente desde el carrito.');
+        } else {
+            console.error('Error al crear el pedido desde el carrito:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+    }
+};
